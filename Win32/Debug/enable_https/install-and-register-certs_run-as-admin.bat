8e77bd22669e3474796a1accbe7bc42d6f132be5
GitStorageOne <mgs2su@gmail.com>
2016-06-16 00:06:57 +0300

    Add files via upload

--- Win32/Debug/enable_https/install-and-register-certs_run-as-admin.bat
+++ Win32/Debug/enable_https/install-and-register-certs_run-as-admin.bat
@@ -7,6 +7,6 @@ certutil -f -p 100500 -importpfx "My" "%~dp0mORMot-SSL-Test-All.p12"
 
 :: Add SSL port
 :: You can use "netsh http show sslcert" to ensure that port was added.
-netsh http add sslcert ipport=0.0.0.0:%SSLPort% certhash=b10e64f26835fe52978d9a1e618f4288277ded91 appid={AA4AC37D-B812-46A7-BEFB-A68167A05BA7}
+netsh http add sslcert ipport=0.0.0.0:%SSLPort% certhash=9707a1065f2be25fc8d6f634f66196e151f49625 appid={AA4AC37D-B812-46A7-BEFB-A68167A05BA7}
 
 pause
--- Win32/Debug/enable_https/unregister-and-uninstall-certs_run-as-admin.bat
+++ Win32/Debug/enable_https/unregister-and-uninstall-certs_run-as-admin.bat
@@ -7,7 +7,7 @@ netsh http delete sslcert ipport=0.0.0.0:%SSLPort%
 
 :: Remove certificates
 :: You can use certmgr.msc and certlm.msc to ensure that sertificates was removed.
-certutil -delstore "My"   "b1 0e 64 f2 68 35 fe 52 97 8d 9a 1e 61 8f 42 88 27 7d ed 91"
+certutil -delstore "My"   "97 07 a1 06 5f 2b e2 5f c8 d6 f6 34 f6 61 96 e1 51 f4 96 25"
 certutil -delstore "Root" "23 ee 2b 14 92 bb b8 80 34 79 89 7c 88 fa 0b 09 80 d2 e3 0f"
 
 :: Remove URL listener from windows httpapi (temporary solution).
